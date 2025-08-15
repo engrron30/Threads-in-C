936326b220198b736feb678a16222946082d8dae
engrron30 <ronaldtorresrosete30.com>
2025-08-15 11:27:29 +0800

    Update example file with pthread_join

--- example.c
+++ example.c
@@ -3,15 +3,21 @@
 
 void* foo(void* arg) {
     printf("Created a new thread");
+    fflush(stdout);
+
     return NULL;
  }
 int main() {
-    
+
     // Create a pthread_t variable to store
     // thread ID
     pthread_t thread1;
-    
+
     // Creating a new thread. 
     pthread_create(&thread1, NULL, foo, NULL);
+
+    // Create join to wait finish doing the thread before continuing or exiting the program.
+    pthread_join(thread1, NULL);
+
     return 0;
 }
