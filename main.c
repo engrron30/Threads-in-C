6aa6c971c0f10d2a1b0930902e824e0c0822f75d
engrron30 <ronaldtorresrosete30.com>
2025-08-15 11:10:45 +0800

    Add pthread_join to display output

--- main.c
+++ main.c
@@ -7,6 +7,8 @@ int main()
 {
     pthread_t thread_monitor_reboot;
     pthread_create(&thread_monitor_reboot, NULL, monitor_reboot, NULL);
+    pthread_join(thread_monitor_reboot, NULL);
+
     return 0;
 }
 
