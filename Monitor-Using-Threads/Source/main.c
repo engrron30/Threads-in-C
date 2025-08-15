56b8eb564d29bb7392e3911760ae4a61177a36c5
engrron30 <ronaldtorresrosete30.com>
2025-08-15 12:08:09 +0800

    Update monitor string checker

--- Monitor-Using-Threads/Source/main.c
+++ Monitor-Using-Threads/Source/main.c
@@ -7,7 +7,7 @@
 #define ARGS_MONITOR_STR_IND    1
 #define STRING_LIMIT            1024
 
-static bool check_monitor_string_from_args(int argc, char *save_here, char *argv[]);
+static bool check_monitor_string_from_args(int argc, char *argv[], char *save_here);
 static void *monitor_string( void *arg );
 
 int main(int argc, char *argv[])
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
     pthread_t thread_monitor_string;
     char string_to_monitor[STRING_LIMIT];
 
-    check_monitor_string_from_args(argc, string_to_monitor, argv);
+    check_monitor_string_from_args(argc, argv, string_to_monitor);
 
     pthread_create(&thread_monitor_string, NULL, monitor_string, NULL);
     pthread_join(thread_monitor_string, NULL);
@@ -23,14 +23,20 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-static bool check_monitor_string_from_args(int argc, char *save_here, char *argv[])
+static bool check_monitor_string_from_args(int argc, char *argv[], char *save_here)
 {
     bool rv = false;
 
-    if (argc > ARGS_MONITOR_STR_IND) {
-        printf("Monitor String: %s\n", argv[ARGS_MONITOR_STR_IND]);
+    if (argc <= ARGS_MONITOR_STR_IND) {
+        perror("Lack arguments: monitor_string <string-to-monitor>\n");
+        goto exit;
     }
 
+    printf("Monitor String: %s\n", argv[ARGS_MONITOR_STR_IND]);
+    strcpy(save_here, argv[ARGS_MONITOR_STR_IND]);
+    rv = true;
+
+exit:
     return rv;
 }
 
