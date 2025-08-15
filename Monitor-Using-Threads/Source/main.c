5488710390b11f8ac02db84cfaeed7ed69427867
engrron30 <ronaldtorresrosete30.com>
2025-08-15 14:37:49 +0800

    Add comments

--- Monitor-Using-Threads/Source/main.c
+++ Monitor-Using-Threads/Source/main.c
@@ -19,6 +19,10 @@ int main(int argc, char *argv[])
     pthread_t thread_monitor_string;
     char string_to_monitor[STRING_LIMIT];
 
+    /* Simply check if there is a string argument when this is executed.
+     * Save the string to string_to_monitor if existing.
+     * Exit if not defined.
+     */
     if (check_monitor_string_from_args(argc, argv, string_to_monitor) != RETURN_SUCCESS) {
         fprintf(stderr, "Lack arguments: monitor_string <string-to-monitor>\n");
         goto exit;
@@ -27,7 +31,7 @@ int main(int argc, char *argv[])
     /* Create a thread called thread_monitor_string.
      * This thread accepts a string called string_to_monitor with typecast of void*.
      * This thread also accepts function called monitor_string and the string we 
-     * added later will be accepted as parameter to monitor_string.
+     * added previously will be accepted as parameter to monitor_string.
      */
     if (pthread_create(&thread_monitor_string, NULL, monitor_string, (void*) string_to_monitor) != 0) {
         perror("Failed to create pthread");
