47e4de7c5378092e4d11606738cdf99666d3aa41
engrron30 <ronaldtorresrosete30.com>
2025-08-15 14:04:29 +0800

    Update main structure

--- Monitor-Using-Threads/Source/main.c
+++ Monitor-Using-Threads/Source/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 #include <pthread.h>
 
 #define ARGS_COMMAND_STR_IND    0
 #define ARGS_MONITOR_STR_IND    1
+#define RETURN_SUCCESS          true
+#define RETURN_FAILURE          false
 #define STRING_LIMIT            1024
 
 static bool check_monitor_string_from_args(int argc, char *argv[], char *save_here);
@@ -12,29 +15,34 @@ static void *monitor_string( void *arg );
 
 int main(int argc, char *argv[])
 {
+    bool rv = EXIT_FAILURE;
     pthread_t thread_monitor_string;
     char string_to_monitor[STRING_LIMIT];
 
-    check_monitor_string_from_args(argc, argv, string_to_monitor);
+    if (check_monitor_string_from_args(argc, argv, string_to_monitor) != RETURN_SUCCESS) {
+        fprintf(stderr, "Lack arguments: monitor_string <string-to-monitor>\n");
+        goto exit;
+    }
 
     pthread_create(&thread_monitor_string, NULL, monitor_string, NULL);
     pthread_join(thread_monitor_string, NULL);
+    rv = EXIT_SUCCESS;
 
-    return 0;
+exit:
+    return rv;
 }
 
 static bool check_monitor_string_from_args(int argc, char *argv[], char *save_here)
 {
-    bool rv = false;
+    bool rv = RETURN_FAILURE;
 
-    if (argc <= ARGS_MONITOR_STR_IND) {
-        perror("Lack arguments: monitor_string <string-to-monitor>\n");
+    if (argv[ARGS_MONITOR_STR_IND] == NULL) {
         goto exit;
     }
 
     printf("Monitor String: %s\n", argv[ARGS_MONITOR_STR_IND]);
     strcpy(save_here, argv[ARGS_MONITOR_STR_IND]);
-    rv = true;
+    rv = RETURN_SUCCESS;
 
 exit:
     return rv;
