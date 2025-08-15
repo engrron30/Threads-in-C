0fe6635b7d055802a38df451890bcde38f04571f
engrron30 <ronaldtorresrosete30.com>
2025-08-15 11:14:27 +0800

    Update monitor_string instead of monitor_reboot

--- main.c
+++ main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <pthread.h>
 
-static void *monitor_reboot( void *arg);
+static void *monitor_string( void *arg );
 
 int main()
 {
-    pthread_t thread_monitor_reboot;
-    pthread_create(&thread_monitor_reboot, NULL, monitor_reboot, NULL);
-    pthread_join(thread_monitor_reboot, NULL);
+    pthread_t thread_monitor_string;
+    pthread_create(&thread_monitor_string, NULL, monitor_string, NULL);
+    pthread_join(thread_monitor_string, NULL);
 
     return 0;
 }
 
-static void *monitor_reboot( void *arg)
+static void *monitor_string( void *arg )
 {
     printf("Monitoring reboot...\n");
 
