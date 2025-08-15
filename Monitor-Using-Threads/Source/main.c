6a1ff1c40b3c5d8ca287b5bc2a1a72ad84efe307
engrron30 <ronaldtorresrosete30.com>
2025-08-15 11:37:27 +0800

    Add monitor string variable

--- Monitor-Using-Threads/Source/main.c
+++ Monitor-Using-Threads/Source/main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <pthread.h>
 
+#define STRING_LIMIT        1024
+
 static void *monitor_string( void *arg );
 
 int main()
 {
     pthread_t thread_monitor_string;
+    char string_to_monitor[STRING_LIMIT];
+
     pthread_create(&thread_monitor_string, NULL, monitor_string, NULL);
     pthread_join(thread_monitor_string, NULL);
 
