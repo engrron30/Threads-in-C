a39ceaf8b10e2eac70c995fed65cbd8f56187c6b
engrron30 <ronaldtorresrosete30.com>
2025-08-15 11:01:09 +0800

    Add main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <pthread.h>
+
+static void *monitor_reboot( void *arg);
+
+int main()
+{
+    pthread_t thread_monitor_reboot;
+    pthread_create(&thread_monitor_reboot, NULL, monitor_reboot, NULL);
+    return 0;
+}
+
+static void *monitor_reboot( void *arg)
+{
+    printf("Monitoring reboot...\n");
+
+    return NULL;
+}
